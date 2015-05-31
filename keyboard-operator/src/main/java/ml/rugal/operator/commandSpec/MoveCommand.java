e008124e446fbf64fe22de7cf1d3d451dd4834cb
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-05-31 18:00:54 -0500

    two move command syntaxes Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- keyboard-operator/src/main/java/ml/rugal/operator/commandSpec/MoveCommand.java
+++ keyboard-operator/src/main/java/ml/rugal/operator/commandSpec/MoveCommand.java
@@ -7,9 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Command for move on screen.
+ * Command for move on screen.<BR/>
+ * Format below are allowed:<BR/>
+ * 1. &lt; key-word &gt; direction [step]<BR/>
+ * 2. direction [step]<BR/>
  *
  * @author Rugal Bernstein
+ * @since 0.1
  */
 public class MoveCommand extends Command
 {
@@ -21,13 +25,13 @@ public class MoveCommand extends Command
 
     private static final Logger LOG = LoggerFactory.getLogger(MoveCommand.class.getName());
 
-    private Move move;
+    private Move direction;
 
     private int step = 1;
 
     public MoveCommand(Move move)
     {
-        this.move = move;
+        this.direction = move;
     }
 
     protected MoveCommand()
@@ -35,31 +39,57 @@ public class MoveCommand extends Command
     }
 
     /**
-     * Here move command only allow several directions of move, this will affect
-     * behavior in motion. Moving steps is in third element if defined.
+     * Here move command only allows moving towards several directions, this
+     * will decide motion behavior.
      *
-     * @param textCommand
+     * @param textCommand the complete command text String
      */
     @Override
     public void init(String[] textCommand)
     {
-        this.move = Move.valueOf(textCommand[1].toUpperCase());
-        if (textCommand.length < 3)
+        int directionIndex = 0;
+        if (isKeyWord(textCommand[0]))
         {
-            LOG.debug("Less than 3 parameters");
+            directionIndex = 1;
+        }
+
+        this.direction = Move.valueOf(textCommand[directionIndex].toUpperCase());
+        if (textCommand.length <= directionIndex + 1)
+        {
+            LOG.debug("No step parameter, using default step length: " + this.step);
             return;
         }
-        this.step = Integer.parseInt(textCommand[2]);
+        //What if it is English word?
+        //TODO parse English word number to digit number
+        this.step = Integer.parseInt(textCommand[directionIndex + 1]);
     }
 
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void executeCommand(Robot robot, List<Command> history)
     {
-        LOG.debug("Button pressing " + this.move.toString() + " " + step + " time(s)");
+        LOG.debug("Button pressing " + this.direction.toString() + " " + step + " time(s)");
         for (int i = 0; i < step; i++)
         {
-            robot.keyPress(this.move.getKeyEvent());
+            robot.keyPress(this.direction.getKeyEvent());
         }
+    }
+
+    private boolean isKeyWord(String word)
+    {
+        boolean value = false;
+        for (String keyword : COMMAND_MAP)
+        {
+            if (word.equalsIgnoreCase(keyword))
+            {
+                value = true;
+                break;
+            }
+        }
+        return value;
     }
 
     public enum Move
