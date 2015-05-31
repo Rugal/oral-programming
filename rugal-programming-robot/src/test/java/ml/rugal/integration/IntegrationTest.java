12ee1a870c3f8aba82892d8457cf870368b74ccb
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-05-30 22:12:08 -0500

    integration test Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- rugal-programming-robot/src/test/java/ml/rugal/integration/IntegrationTest.java
+++ rugal-programming-robot/src/test/java/ml/rugal/integration/IntegrationTest.java
@@ -1,6 +1,7 @@
 package ml.rugal.integration;
 
 import com.google.gson.Gson;
+import java.awt.AWTException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -8,23 +9,27 @@ import javax.sound.sampled.LineUnavailableException;
 import ml.rugal.googlespeech.gson.SpeechResponseData;
 import ml.rugal.googlespeech.request.APIRequest;
 import ml.rugal.microphone.FlacMicrophone;
+import ml.rugal.operator.CommandExecutor;
+import ml.rugal.operator.commandSpec.Command;
+import ml.rugal.operator.commandSpec.CommandFactory;
+import ml.rugal.operator.exception.CommandInvalidException;
+import ml.rugal.operator.exception.CommandNotFoundException;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  *
- * @author Administrator
+ * @author Rugal Bernstein
  */
-public class RequesterTest
+public class IntegrationTest
 {
 
     private final String apikey = "AIzaSyBqC4CJz7HcalA_2aP5bd_Ll8iyLbgxtJs";
 
     private final APIRequest request = new APIRequest(apikey);
 
-    public RequesterTest()
+    public IntegrationTest()
     {
     }
 
@@ -39,18 +44,18 @@ public class RequesterTest
     }
 
     @Test
-    @Ignore
-    public void testExecute() throws IOException, LineUnavailableException, InterruptedException, URISyntaxException
+//    @Ignore
+    public void testExecute() throws IOException, LineUnavailableException, InterruptedException, URISyntaxException, CommandNotFoundException, CommandInvalidException, AWTException
     {
         System.out.println("execute");
-
+        CommandExecutor executor = new CommandExecutor();
         try (FlacMicrophone microphone = new FlacMicrophone())
         {
             File file = new File("test.wav");
-
+//            File out = new File("E:\\Downloads\\move.flac");
             System.out.println("Start");
             microphone.startRecord(file);
-            Thread.sleep(6000);
+            Thread.sleep(4000);
             microphone.close();
             System.out.println("Stop");
             File out = microphone.getFlacFile();
@@ -58,6 +63,8 @@ public class RequesterTest
             String json = prejson.substring(prejson.indexOf("\n") + 1);
             SpeechResponseData ob = new Gson().fromJson(json, SpeechResponseData.class);
             System.out.println(ob.result[0].alternative[0].transcript);
+            Command cmd = CommandFactory.constructCommand(ob.result[0].alternative[0].transcript.split(" "));
+            executor.execute(cmd);
             file.deleteOnExit();
             out.deleteOnExit();
         }
