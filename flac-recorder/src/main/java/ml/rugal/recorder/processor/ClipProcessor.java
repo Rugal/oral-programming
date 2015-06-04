720ac057017d23833f0ebb0e5be8eae012842ed2
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-06-04 14:41:48 -0500

    javadoc for Clipprocessor Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- flac-recorder/src/main/java/ml/rugal/recorder/processor/ClipProcessor.java
+++ flac-recorder/src/main/java/ml/rugal/recorder/processor/ClipProcessor.java
@@ -3,11 +3,17 @@ package ml.rugal.recorder.processor;
 import javax.sound.sampled.AudioInputStream;
 
 /**
- *
+ * The processor class used in microphone data process.
+ * <p>
  * @author Rugal Bernstein
  */
 public abstract class ClipProcessor
 {
 
+    /**
+     * Method to invoke when process audio data in real.
+     * <p>
+     * @param ais
+     */
     public abstract void process(AudioInputStream ais);
 }
